22da53412505be716ed5bf46832838d57f85c69c
TijmenSimons <145433363+VoxturTijmen@users.noreply.github.com>
2023-11-28 23:25:29 +0100

    day 6

--- 2022/day_6/part_1.py
+++ 2022/day_6/part_1.py
@@ -4,7 +4,29 @@ __info = [2022, 6, 1]
 
 def main():
     data = get_input(*__info)
-    data = """""".split("\n")
+#     data = """bvwbjplbgvbhsrlpgdmjqwftvncz
+# nppdvjthqldpwncqszvftbrmjlhg
+# nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+# zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+# """.split("\n")
+    
+    def compare(idx, string):
+        ltrs = string[idx:idx+4]
+        
+        new_ltrs = {ltr: ltr for ltr in ltrs}
+
+        return len(new_ltrs) == 4
+
+    for datastream in data[:-1]:
+        max_ = len(datastream)
+        idx = 0
+        while not compare(idx, datastream):
+            idx += 1
+            if max_ < idx:
+                print("broken")
+                break
+        print(idx+4)
+
     store_output(data, *__info)
 
 
--- 2022/day_6/part_2.py
+++ 2022/day_6/part_2.py
@@ -4,7 +4,31 @@ __info = [2022, 6, 2]
 
 def main():
     data = get_input(*__info)
-    data = """""".split("\n")
+#     data = """bvwbjplbgvbhsrlpgdmjqwftvncz
+# nppdvjthqldpwncqszvftbrmjlhg
+# nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+# zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+# """.split("\n")
+    
+    key_len = 14
+    
+    def compare(idx, string):
+        ltrs = string[idx:idx+key_len]
+        
+        new_ltrs = {ltr: ltr for ltr in ltrs}
+
+        return len(new_ltrs) == key_len
+
+    for datastream in data[:-1]:
+        max_ = len(datastream)
+        idx = 0
+        while not compare(idx, datastream):
+            idx += 1
+            if max_ < idx:
+                print("broken")
+                break
+        print(idx+key_len)
+
     store_output(data, *__info)
 
 
